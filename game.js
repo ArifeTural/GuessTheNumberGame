f0ed152b0af29cbd3f93d93c3f12ac63849c9c7a
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-07-30 21:13:39 +0300

    bitti

--- game.js
+++ game.js
@@ -1,9 +1,5 @@
 const yazilanYer = document.querySelector("#guess")
-console.log(yazilanYer);
 const button = document.querySelector("#guessSend")
-console.log(button);
-const attention = document.querySelector("#attention")
-const sayilar = document.querySelector("#sayilar span")
 const tekrar = document.querySelector("#tekrar")
 
 
@@ -23,23 +19,20 @@ let random
 random = rasgele(0,100)
 
 button.addEventListener("click",() => {
-    if(yazilanYer.value == ""){
-        attention.textContent = "bir sayı giriniz"
-        kalan++
-    }
-    else if( yazilanYer.value == random){
-        attention.textContent = "Tebrikler doğru cevap" + random
+
+if( yazilanYer.value === random){
+        attention.textContent = "Tebrikler doğru cevap :" + random
         tekrar.style.display = "block"
         button.disabled = true
        
    }
    else if(yazilanYer.value > random){
     attention.textContent = "Yanlış cevap. Tahmininz sayıdan büyük. sayı: " + random
-    tahminler.push(" "  + yazilanYer.value)
+   
    }
    else if(yazilanYer.value < random){
     attention.textContent = "Yanlış cevap. Tahmininz sayıdan küçük. sayı: " + random
-    tahminler.push(" "  + yazilanYer.value)
+    
    }
 
   yazilanYer.focus()
